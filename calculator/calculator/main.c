ebd860f07580d111d22def44390c9abeba8d7d77
sara <sara.lekouara.dz@viacesi.fr>
2017-11-05 16:36:54 +0000

    change2 main

--- calculator/calculator/main.c
+++ calculator/calculator/main.c
@@ -8,10 +8,8 @@ int addition(int a, int b)
 int main()
 
 {
-<<<<<<< HEAD
-   printf("a+b=%d\n", addition(1,4));
-=======
+
     printf("L’addition de a et de b vaut %d\n", addition(1,4));
->>>>>>> baaa5e963558e90a7f0e23d7e6847c5660e28da9
+
     return 0;
 }
